5146f5df44870c7b4c9252e163dfc67f2919a00a
Andreas Johansson <andreas@ibuypink.com>
2012-03-05 13:06:49 +0100

    Updated description

---
+++ README.md
@@ -0,0 +1,17 @@
+# jQuery.reSetValue.js
+
+## What does it do
+
+Placeholder text for searchfields when placeholder-attribute (HTML5) is not available.
+
+## How to use
+
+Simply add the plugin and then:
+
+`$('#search').reSetValue();`
+
+HTML:
+
+	<input type="text" id="search" value="Search here" />
+
+
---
+++ jquery.reSetValue.js
@@ -0,0 +1,19 @@
+/*
+**  Copyright 2010, Andreas Johansson
+**  Licensed under a MIT license, http://ibuypink.com/mit-license/
+**
+*/
+(function($){ 
+  jQuery.fn.reSetValue = function() {
+    return this.each(function() { 
+      var el = $(this);  
+      var defVal = el.val();
+      el.focus(function() {
+        var newVal = el.val();
+        if (defVal==newVal) el.val('');
+      }).blur(function() {
+        if(el.val()=='') el.val(defVal);
+      });
+    });
+  };
+})(jQuery);
